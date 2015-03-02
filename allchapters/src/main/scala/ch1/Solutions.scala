20fdd80bd71e1302fb2e8c65de424e098f4ee204
gothug <kojuhovskiy@gmail.com>
2015-03-02 23:01:13 +0300

    ch1

--- allchapters/src/main/scala/ch1/Solutions.scala
+++ allchapters/src/main/scala/ch1/Solutions.scala
@@ -1,9 +1,11 @@
 package ch1
 
+import scala.util.{Success, Try}
+
 /**
  * @author Got Hug
  */
-object Solutions extends App {
+object Solutions {
   def compose[A, B, C](g: B => C, f: A => B): A => C = {
     x => g(f(x))
   }
@@ -14,11 +16,45 @@ object Solutions extends App {
       bb <- b
     } yield (aa, bb)
   }
+
+  def fuseViaMap[A, B](a: Option[A], b: Option[B]): Option[(A, B)] = {
+    a.flatMap(aa => b.map(bb => (aa, bb)))
+  }
+
+  def check[T](xs: Seq[T])(pred: T => Boolean): Boolean = {
+    def loop(xsxs: Seq[T]): Boolean = xsxs match {
+      case h +: t => if (Try(pred(h)) == Success(true)) loop(t) else false
+      case _      => true
+    }
+
+    loop(xs)
+  }
+
+  def permutations(x: String): Seq[String] = {
+    def loop(xs: List[Char]): List[List[Char]] = {
+      if (xs.isEmpty) List(List())
+      else {
+        for {
+          e <- xs
+          es <- loop(xs filter (_ != e))
+        } yield e :: es
+      }
+    }
+
+    (loop(x.toList) map { _.mkString }).toSeq
+  }
+
+  def permutations2(x: String): Seq[String] = {
+    def loop(xs: List[Char]): List[List[Char]] = {
+      if (xs.isEmpty) List(List())
+      else {
+        for {
+          e <- xs
+          es <- loop(xs filter (_ != e))
+        } yield e :: es
+      }
+    }
+
+    (loop(x.toList) map { _.mkString }).toSeq
+  }
 }
-
-//object Solutions extends App {
-//  println("abc")
-//  assert(Solutions.fuse(None, None) == None)
-
-//  fuse(Some(1), Some(2))
-//}
--- allchapters/src/test/scala/ch1/SolutionsTest.scala
+++ allchapters/src/test/scala/ch1/SolutionsTest.scala
@@ -1,14 +1,34 @@
 package ch1
 
 import org.scalatest._
+import Solutions._
 
 /**
  * @author Got Hug
  */
 class SolutionsTest extends WordSpec with Matchers {
   "fuse" should {
-    "work" in {
-      1 shouldBe 1
+    "work correctly" in {
+      fuse(None, None) shouldBe None
+      fuse(Some(1), None) shouldBe None
+      fuse(None, Some(2)) shouldBe None
+      fuse(Some(1), Some(2)) shouldBe Some((1, 2))
+    }
+  }
+
+  "fuseViaMap" should {
+    "work correctly" in {
+      fuseViaMap(None, None) shouldBe None
+      fuseViaMap(Some(1), None) shouldBe None
+      fuseViaMap(None, Some(2)) shouldBe None
+      fuseViaMap(Some(1), Some(2)) shouldBe Some((1, 2))
+    }
+  }
+
+  "check" should {
+    "work correctly" in {
+      check(0 until 10)(40 / _ > 0) shouldBe false
+      check(1 until 10)(40 / _ > 0) shouldBe true
     }
   }
 }
