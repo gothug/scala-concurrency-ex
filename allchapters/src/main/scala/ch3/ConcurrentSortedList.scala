b126288c2395ae0522c5fa370ecab50d29ea6dc1
gothug <kojuhovskiy@gmail.com>
2015-03-17 23:22:02 +0300

    ch3

--- allchapters/src/main/scala/ch3/ConcurrentSortedList.scala
+++ allchapters/src/main/scala/ch3/ConcurrentSortedList.scala
@@ -8,16 +8,24 @@ import java.util.concurrent.atomic.AtomicReference
 class ConcurrentSortedList[T](implicit val ord: Ordering[T]) {
 
   trait Element
-  case class Node(el: T, next: AtomicReference[Element]) extends Element
+  case class Node(value: T, next: AtomicReference[Element]) extends Element
   case object End extends Element
 
   private val head = new AtomicReference[Element](End)
 
   def add(x: T): Unit = {
-    def loop(el: Element) = el match {
-      case End =>
+    def loop(ref: AtomicReference[Element]): Unit = {
+      val element = ref.get
 
-
+      element match {
+        case Node(value, next) =>
+          if (x > value) {
+            loop(next)
+          } else {
+            val newElement = Node(x, new AtomicReference[Element](next.get))
+            if (!ref.compareAndSet(element, newElement)) loop(ref)
+          }
+      }
     }
 
     loop(head)
