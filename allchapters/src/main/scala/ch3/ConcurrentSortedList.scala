67e5f17d0fbbad2c4356afc57ef0c26d4bd39e27
gothug <kojuhovskiy@gmail.com>
2015-03-18 23:18:23 +0300

    ch3

--- allchapters/src/main/scala/ch3/ConcurrentSortedList.scala
+++ allchapters/src/main/scala/ch3/ConcurrentSortedList.scala
@@ -1,6 +1,7 @@
 package ch3
 
 import java.util.concurrent.atomic.AtomicReference
+import scala.concurrent.forkjoin.ForkJoinPool
 
 /**
  * @author Got Hug
@@ -18,18 +19,65 @@ class ConcurrentSortedList[T](implicit val ord: Ordering[T]) {
       val element = ref.get
 
       element match {
-        case Node(value, next) =>
-          if (x > value) {
-            loop(next)
-          } else {
-            val newElement = Node(x, new AtomicReference[Element](next.get))
-            if (!ref.compareAndSet(element, newElement)) loop(ref)
-          }
+        case Node(value, next) if ord.gt(x, value) =>
+          loop(next)
+        case _ =>
+          val newElement = Node(x, new AtomicReference[Element](element))
+          if (!ref.compareAndSet(element, newElement)) loop(ref)
       }
     }
 
     loop(head)
   }
 
-//  def iterator: Iterator[T] = ???
+  def iterator: Iterator[T] = {
+    new Iterator[T] {
+      var ptr: Element = head.get
+
+      def hasNext = {
+        ptr match {
+          case End => false
+          case _   => true
+        }
+      }
+
+      def next(): T = {
+        ptr match {
+          case Node(value, next) =>
+            ptr = next.get
+            value
+          case End =>
+            throw new Exception("empty iterator")
+        }
+      }
+    }
+  }
+}
+
+object ConcurrentSortedList extends App {
+  val list = new ConcurrentSortedList[Int]
+
+  val pool = new ForkJoinPool(16)
+
+  for (i <- 1 to 16) {
+    val r = new Runnable {
+      override def run(): Unit = {
+        for (i <- 1 to 1000) {
+          list.add(scala.util.Random.nextInt(100))
+        }
+//        println("run")
+//        Thread.sleep(1000)
+      }
+    }
+
+    pool.execute(r)
+//    ExecutionContext.global.execute(r)
+  }
+
+  Thread.sleep(5000)
+
+  val nums = list.iterator.toList
+
+  println(nums.size)
+  assert(nums == nums.sorted)
 }
